7482c76c7a2de90abff955dc48ae4568e5158934
Galo DeSalazar <gdesalazar@gmail.com>
2019-08-14 16:09:39 -0400

    Added eventlistner for email input and making new url

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import {data} from "./seed_data";
-
-document.getElementById('test').innerHTML =  'hello'
-
+// overview of code structure inspired by catena developed by clericl github
+// import data from "./seed_data";
 // data returns an array of objects
+
+const searchInput = document.querySelector("#email")
+
+
+searchInput.addEventListener("keydown", event=>{
+    let email;
+    let newUrl = 'https://haveibeenpwned.com/api/v3/breachedaccount/';
+debugger
+    if(event.key === "Enter" && searchInput.value.length > 0){
+        event.preventDefault();
+        email = searchInput.value;
+        email = email.replace(/\s/g, '');
+        newUrl += email;
+        console.log(email);
+        console.log(newUrl);
+    }else{
+        console.log("Please enter email");
+    }
+})
 
 
 // const hibpApiKey = '[03a794521329432bad12af5f5bc6db3e]';
 // let keyHeader = new Headers();
 // keyHeader.append('Hibp-Api-Key', hibpApiKey)
 
-// let email = 'gdesalazar@gmail.com';
-// let newUrl = 'https://haveibeenpwned.com/api/v3/breachedaccount/' + email;
 
 
 // fetch(newUrl,{method: "GET", headers: keyHeader})
