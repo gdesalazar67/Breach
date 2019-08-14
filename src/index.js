d356aa06fca1c430fda29b78d551929d0311ba48
Galo DeSalazar <gdesalazar@gmail.com>
2019-08-14 00:34:27 -0400

    Added api key

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+document.getElementById('test').innerHTML =  'hello'
+
+const hibpApiKey = '[03a794521329432bad12af5f5bc6db3e]';
+let keyHeader = new Headers();
+keyHeader.append('Hibp-Api-Key', hibpApiKey)
+
+let email = 'gdesalazar@gmail.com';
+let newUrl = 'https://haveibeenpwned.com/api/v3/breachedaccount/' + email;
+
+
+fetch(newUrl,{method: "GET", headers: keyHeader})
+    .then(res => res.json())
+    .then(function(data){
+       console.log(data)
+    })
+
+
+// api key 03a794521329432bad12af5f5bc6db3e
+// hibp-api-key: [03a794521329432bad12af5f5bc6db3e]
