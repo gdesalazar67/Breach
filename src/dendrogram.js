40f3328a21454247ab132b55e48b3c73a8efe7a6
Galo DeSalazar <gdesalazar@gmail.com>
2019-08-15 13:52:47 -0400

    Added links to nodes

--- src/dendrogram.js
+++ src/dendrogram.js
@@ -9,7 +9,7 @@ let svg = d3.select("#svg")
     .append("g").attr("transform", "translate(50,50)");
 
 const resultsTree = (hiroData)=>{
-    let treeStructure = d3.tree().size([550, 400]);
+    let treeStructure = d3.tree().size([300, 300]);
     let information = treeStructure(hiroData);
     console.log(information.descendants());
     console.log(information.links());
@@ -18,7 +18,19 @@ const resultsTree = (hiroData)=>{
       circles.enter().append("circle")
         .attr("cx", function(d){return d.x;})
         .attr("cy", function(d){return d.y;})  
-        .attr("r",5);
+        .attr("r",5)
+        .attr('fill', 'blue');
+    let connections =svg.append("g").selectAll("path")
+        .data(information.links());
+    connections.enter().append("path")
+        .attr("d", function(d){
+            return "M" + d.source.x + "," + d.source.y + " C " +
+            d.source.x + "," + (d.source.y + d.target.y)/2 + " " +
+            d.target.x + "," + (d.source.y + d.target.y)/2 + " " +
+            d.target.x + "," + d.target.y;
+        })
+        .attr('stroke', 'red')
+        .attr('fill', 'none');
 } 
 
 
