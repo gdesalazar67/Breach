2c373880e1d3172dffedfcb3d5e19426c199bcbe
Galo DeSalazar <gdesalazar@gmail.com>
2019-08-15 13:13:00 -0400

    Added circles to tree

--- src/dendrogram.js
+++ src/dendrogram.js
@@ -5,13 +5,20 @@ let w = 600, h = 600, pad = 50;
 let svg = d3.select("#svg") 
     .append("svg") 
     .attr("height", h)
-    .attr("width", w);
+    .attr("width", w)
+    .append("g").attr("transform", "translate(50,50)");
 
 const resultsTree = (hiroData)=>{
-    let treeStructure = d3.tree().size([600, 600]);
+    let treeStructure = d3.tree().size([550, 400]);
     let information = treeStructure(hiroData);
     console.log(information.descendants());
     console.log(information.links());
+    let circles = svg.append("g").selectAll("circle")
+        .data(information.descendants());
+      circles.enter().append("circle")
+        .attr("cx", function(d){return d.x;})
+        .attr("cy", function(d){return d.y;})  
+        .attr("r",5);
 } 
 
 
