0b7394860f5e40887b87a086ba4b30849b7f87ff
Galo DeSalazar <gdesalazar@gmail.com>
2019-08-16 19:21:16 -0400

    Added cors-anywhere to connect to api and retrive data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // overview of code structure inspired by catena developed by clericl github
-// import data from "./seed_data";
-// data returns an array of objects
+//cors-anywhere used https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe/43881141
 import * as Data from "./seed_data";
 // import {resultsTree} from "./dendrogram";
 
@@ -9,16 +8,33 @@ const searchInput = document.querySelector("#email")
 
 searchInput.addEventListener("keydown", event=>{
     let email;
-    let newUrl = 'https://haveibeenpwned.com/api/v3/breachedaccount/';
-
+    let newUrl = 'https://cors-anywhere.herokuapp.com/https://haveibeenpwned.com/api/v3/breachedaccount/';
+   
     if(event.key === "Enter" && searchInput.value.length > 0){
         event.preventDefault();
         email = searchInput.value;
         email = email.replace(/\s/g, '');
-        newUrl += email;
+        newUrl += email + "?truncateResponse=false";
         
         let hiroData = Data.childParentData(email, Data.data); 
+        //////////////////////////////////
+        const hibpApiKey = '03a794521329432bad12af5f5bc6db3e';
+        let keyHeaders = new Headers();
+        debugger
+        keyHeaders.append('Hibp-Api-Key', hibpApiKey)
         
+        fetch(newUrl, { method: "GET", headers: keyHeaders })
+            .then(res => res.json())
+            .then(function (data) {
+
+                console.log(data)
+            })
+            .catch(error => {
+
+                console.log(error)
+            })
+
+            ///////////////////////////////
         resultsTree(hiroData);
     }else{
         console.log("Please enter email");
@@ -35,13 +51,13 @@ searchInput.addEventListener("keydown", event=>{
 // fetch(newUrl,{method: "GET", headers: keyHeader})
 //     .then(res => res.json())
 //     .then(function(data){
-//         
+        
 //        console.log(data)
 //     })
 //     .catch(error => {
-//         
+        
 //         console.log(error)
-//     }
+//     })
         
 
 
