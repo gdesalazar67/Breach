977083a56bbdfba5fbf341e0868456e2a30537ab
Galo DeSalazar <gdesalazar@gmail.com>
2019-08-18 13:41:35 -0400

    Added pop up window to leaves

--- src/dendrogram.js
+++ src/dendrogram.js
@@ -14,18 +14,61 @@ const resultsTree = (hiroData)=>{
    
     let treeStructure = d3.tree().size([svgDiv.clientWidth, 800]);
     let information = treeStructure(hiroData);
-    console.log(information)
+    let leaves = information.leaves();
 
+    ///append div to svg for popup window 
+    let div = d3.select(svgDiv).append("div")
+        .attr("class", "tooltip")
+        .style("display", "none");
+
+
+    ///pop up window functions 
+    const mouseover = ()=> {
+        div.style("display", "inline");
+    }
+
+    const mousemove =()=> {
+       
+        div
+            .text(d3.event.pageX + "," + d3.event.pageY)
+            .style("left", (d3.event.pageX - 34) + "px")
+            .style("top", (d3.event.pageY - 12) + "px");
+    }
+
+    const mouseout =()=> {
+        div.style("display", "none");
+    }
+    // create leaf circles 
     let circles = svg.append("g").selectAll("circle")
-        .data(information.descendants());
-    console.log(information.descendants())
-
+        .data(leaves);
+    
       circles.enter().append("circle")
+        .attr('class', 'leaves')
         .attr("cx", function(d){return d.x;})
         .attr("cy", function(d){return d.y;})  
-        .attr("r",20)
-        .attr('fill', d => (d.depth === 1)? "red": "blue");
-    console.log(svg.node().getBBox())
+        .attr("r",10)
+        .attr('fill', "blue")
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseout", mouseout);
+
+
+    // d3.select("leaves")
+    //     .selectAll("div")
+    //     .data(leaves)
+    //     .enter().append("div")
+    //     .style("width", 20)
+    //     // .text(function (d) { return d.data.details; })
+    //     .on("mouseover", function (d) { tooltip.text(d); return tooltip.style("visibility", "visible"); })
+    //     .on("mousemove", function () { return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px"); })
+    //     .on("mouseout", function () { return tooltip.style("visibility", "hidden"); });
+    
+
+    // console.log(svg.node().getBBox())
+
+
+
+
     let connections =svg.append("g").selectAll("path")
         .data(information.links());
 
