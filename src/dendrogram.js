dfbbe6ef93dd6c93cc9f496ae32e576b0fbf0822
Galo DeSalazar <gdesalazar@gmail.com>
2019-08-18 19:17:50 -0400

    Added rectangles and popup up window

--- src/dendrogram.js
+++ src/dendrogram.js
@@ -13,8 +13,14 @@ let svg = d3.select(svgDiv)
 const resultsTree = (hiroData)=>{
    
     let treeStructure = d3.tree().size([svgDiv.clientWidth, 800]);
+    //hierarchy breakdown
     let information = treeStructure(hiroData);
     let leaves = information.leaves();
+    let parents= noLeaves(information.descendants());
+
+    //test
+    console.log(information.descendants());
+    console.log(noLeaves(information.descendants()));
 
     // append div to svg for popup window 
     let div = d3.select(svgDiv).append("div")
@@ -27,7 +33,6 @@ const resultsTree = (hiroData)=>{
     }
 
     const mousemove =(data)=> {
-        debugger
         div
             .text(data.data.child)
             .style("left", (d3.event.pageX - 34) + "px")
@@ -52,23 +57,25 @@ const resultsTree = (hiroData)=>{
         .on("mousemove", d=>mousemove(d))
         .on("mouseout", mouseout);
 
+// create reactangles for parents
+    let rects = svg.append("g").selectAll("rect")
+        .data(parents);
+        debugger
+        
+    rects.enter().append("rect")
+            .attr('class', "Reacts")
+            .attr("x", d =>(d.x))
+            .attr("y", d =>(d.y))
+            .attr("width", 50)
+            .attr("height", 30)
+            .on("mouseover", mouseover)
+            .on("mousemove", d => mousemove(d))
+            .on("mouseout", mouseout);
 
-    // d3.select("leaves")
-    //     .selectAll("div")
-    //     .data(leaves)
-    //     .enter().append("div")
-    //     .style("width", 20)
-    //     // .text(function (d) { return d.data.details; })
-    //     .on("mouseover", function (d) { tooltip.text(d); return tooltip.style("visibility", "visible"); })
-    //     .on("mousemove", function () { return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px"); })
-    //     .on("mouseout", function () { return tooltip.style("visibility", "hidden"); });
-    
-
-    // console.log(svg.node().getBBox())
 
 
 
-
+//Build paths connecting child and parent 
     let connections =svg.append("g").selectAll("path")
         .data(information.links());
 
@@ -82,6 +89,7 @@ const resultsTree = (hiroData)=>{
         .attr('stroke', 'red')
         .attr('fill', 'none');
 
+///append text to tree nodes
     let names = svg.append("g").selectAll("text")
         .data(information.descendants());
         
@@ -92,4 +100,13 @@ const resultsTree = (hiroData)=>{
         .attr('fill', 'black');
 
 } 
+
+const noLeaves = (root)=>{
+    return root.filter(obj=> {
+        return (obj.depth !== 3);
+    });
+};
+
+
+
 
