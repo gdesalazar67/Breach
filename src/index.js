6897723d83f2a0be34f7b55deaedb269bb41fcdb
Galo DeSalazar <gdesalazar@gmail.com>
2019-08-28 20:15:27 -0400

    Fixed more css

--- src/index.js
+++ src/index.js
@@ -35,9 +35,7 @@ searchInput.addEventListener("keydown", event=>{
         // resultsTree(hiroData);
 
         //////////////////////////////////
-        let intro = document.getElementById("intro");
-            intro.style.display = "none"
-
+       
         const hibpApiKey = '03a794521329432bad12af5f5bc6db3e';
         let keyHeaders = new Headers();
         keyHeaders.append('Hibp-Api-Key', hibpApiKey)
@@ -47,7 +45,9 @@ searchInput.addEventListener("keydown", event=>{
         .then(function (data) {
             console.log(data);
             let hiroData = Data.childParentData(email, data); 
-            
+            let intro = document.getElementById("intro");
+            intro.style.display = "none";
+
             resultsTree(hiroData); 
             
             })
