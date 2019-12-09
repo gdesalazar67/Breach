da7cd7e9f408ee17054f3968fcea1ad9f7761ca9
Galo DeSalazar <gdesalazar@gmail.com>
2019-12-08 23:09:41 -0800

    added color picker for years

--- src/scatter_plot.js
+++ src/scatter_plot.js
@@ -44,13 +44,14 @@ const buildChart = (dataSet) => {
         .attr("viewBox", `0 0 ${viewWidth} ${viewHeight}`);
 
     buildYaxis(svg, viewHeight);
+    buildBubbleChart(svg, viewWidth, viewHeight);
     
 };
 
 const buildYaxis = (svg, height) => {
 
     height = height === 3576 ? 3525: 4709;
-    
+
     let yScale = d3.scaleLinear()
         .domain([2020, 2009])
         .range([0, height])
@@ -88,8 +89,50 @@ const buildYaxis = (svg, height) => {
 };
     
 
-const buildBubbleChart = () => {
+const buildBubbleChart = (svg, width, height) => {
 
+    let root = {children: majorBreaches["2009"]}; 
+    let flatNodeHeirarchy =  d3.hierarchy(root).sum(d => d.recordsLost);
+   
+    let packedDataBuilder = () => {
+       
+        let pack = d3.pack()
+            .size([width, 325])
+            .padding(1)
+            return pack(flatNodeHeirarchy);
+    };
+
+    let packedData = packedDataBuilder();
+    console.log(packedData)
+
+    let leaf = svg.append("g")
+        .attr("class", "bubbecluster")
+        .attr("transform", `translate(0,1500)`)
+        .selectAll("g")
+        .data(packedData.leaves())
+        .enter()
+        .append("g")
+        .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`);
+
+    
+    const color = d3.scaleOrdinal()
+        .domain([2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019])
+        .range(["#59a6f81f", "#e3eef9", "#cfe1f2", "#b5d4e9", "#93c3df", "#6daed5", "#4b97c9", "#2f7ebc", "#1864aa", "#0a4a90", "#08306b"].reverse());
+
+    const circle = leaf.append("circle")
+        .attr("r", d => d.r)
+        .attr("class", "r-circles")
+        .attr("fill", d => {
+            return color(d.data.year);
+        });
+    
+    // creat some circles//
+    // svg.selectAll("div")
+    //     .data(majorBreaches["2009"])
+    //     .enter()
+    //     .append()
+    
+    // var simulation = d3.forceSimulation(data).force("charge", d3.forceManyBody().strength([-50])).force("x", d3.forceX()).force("y", d3.forceY()).on("tick", ticked);
 };
     
     
