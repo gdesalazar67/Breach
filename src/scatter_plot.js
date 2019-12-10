6b857984ce3fff52c1c539b2cd3c4d27bb9df48f
Galo DeSalazar <gdesalazar@gmail.com>
2019-12-09 19:15:56 -0800

    added .raise function to hover and transition

--- src/scatter_plot.js
+++ src/scatter_plot.js
@@ -95,29 +95,15 @@ const buildBubbleChart = (svg, width, height) => {
 
     const sizeScale = d3.scaleSqrt()
         .range([17, 60]);
+
+    sizeScale.domain(d3.extent(classes, d => d.recordsLost));
     
     let pack = d3.pack()
-    .size([width, 325])
-    // .padding(1)
-    
-    sizeScale.domain(d3.extent(classes, d => d.recordsLost));
+        .size([width, 325]);
     
     pack.radius(d => sizeScale(d.value)); 
 
     let root =  d3.hierarchy({children: classes}).sum(d => d.recordsLost);
-    // let packedDataBuilder = () => {
-       
-    //     let pack = d3.pack()
-    //         .size([width, 325])
-    //         .padding(1)
-
-    //     return pack(flatNodeHeirarchy);
-
-    // };
-
-    // let packedData = packedDataBuilder(flatNodeHeirarchy);
-    // console.log(packedData);
-
 
     let leaf = svg.append("g")
         .attr("class", "bubbecluster")
@@ -128,7 +114,6 @@ const buildBubbleChart = (svg, width, height) => {
         .append("g")
         .attr("transform", d => `translate(${d.x +1},${d.y + 1})`);
 
-    
     const color = d3.scaleOrdinal()
         .domain([2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019])
         .range(["#59a6f81f", "#e3eef9", "#cfe1f2", "#b5d4e9", "#93c3df", "#6daed5", "#4b97c9", "#2f7ebc", "#1864aa", "#0a4a90", "#08306b"]);
@@ -137,30 +122,39 @@ const buildBubbleChart = (svg, width, height) => {
         .domain([17, 60])
         .range(pallete);
 
-    // const size = d3.scaleLinear()
-    //     .domain([1, 100])  // What's in the data
-    //     .range([30, 80])  // Size in pixel
-    //     .clamp(true);
+    // const mouseOver = () => {
+    //     console.log("mouseOver on " + this)
+
+    // }
+        
+
+    // const mouseOut = (circle) => {
+    //     console.log(circle);
+    // };
 
     const circle = leaf.append("circle")
         .attr("r", d => d.r)
         .attr("class", "r-circles")
 
         .attr("stroke", "white")
-        .attr("fill", d => color2(d.value));
+        .attr("fill", d => color2(d.value))
+        // .on("mouseover", mouseOver)
+        .on("mouseover", function (d,i) {
+            console.log("mouseOver on " + this)
+            let parent = this.closest("g");
+            d3.select(parent).raise();
+            d3.select(this)
+                .transition()
+                .duration(200)
+                .attr("r", d => d.r * 2);
 
-    // leaf.append("text")
-    //     .attr("text-anchor", "middle")
-    //     .attr("dy", ".2em")
-    //     .text(function (d) {
-    //         return d.data.entity;
-    //     })
-    //     .attr("font-family", "sans-serif")
-    //     // .attr("font-size", function (d) {
-    //     //     return d.r / 5;
-    //     // })
-    //     .attr("font-size", 10)
-    //     .attr("fill", "white");
+        })
+        .on("mouseout", function (d,i) {
+            d3.select(this)
+                .transition()
+                .duration(200)
+                .attr("r", d => d.r);
+        } );
 
     let format = d3.format = d3.format(",d");
     // leaf.append("clipPath")
@@ -183,8 +177,12 @@ const buildBubbleChart = (svg, width, height) => {
         .attr("y", (d, i, nodes) => `${i - nodes.length / 2 + 0.8}em`)
         .text(d => d);
 
-    leaf.append("title")
-        .text(d => `${d.data.entity}\n${format(d.value)}`);
+    // leaf.append("title")
+    //     .text(d => `${d.data.entity}\n${format(d.value)}`);
+
+// on mouse over
+
+   
 
 
 
@@ -192,46 +190,46 @@ const buildBubbleChart = (svg, width, height) => {
 
 
 
+}
 
         // onclick functions //
    
-    let current_circle = undefined;
+    // let current_circle = undefined;
 
-    function selectOccupation(d) {
-        if(current_circle !== undefined){
-            current_circle.attr("fill", d => color(d.data.year));
-            svg.selectAll("#details-popup").remove();
-        };
+    // function selectOccupation(d) {
+    //     if(current_circle !== undefined){
+    //         current_circle.attr("fill", d => color(d.data.year));
+    //         svg.selectAll("#details-popup").remove();
+    //     };
 
 
-        current_circle = d3.select(this);
-        current_circle.attr("fill", "#b2e1f9");
+    //     current_circle = d3.select(this);
+    //     current_circle.attr("fill", "#b2e1f9");
         
-        let textblock = svg.selectAll("#detail-popup")
-            .data([d])
-            .enter()
-            .append("g")
-            .attr("id", "details-popup")
-            .attr("font-size", 14)
-            .attr("font-family", "sans-serif")
-            .attr("text-anchor", "center")
-            .attr("transform", d => `translate(0, 20)`);
+    //     let textblock = svg.selectAll("#detail-popup")
+    //         .data([d])
+    //         .enter()
+    //         .append("g")
+    //         .attr("id", "details-popup")
+    //         .attr("font-size", 14)
+    //         .attr("font-family", "sans-serif")
+    //         .attr("text-anchor", "center")
+    //         .attr("transform", d => `translate(0, 20)`);
 
-        textblock.append("text")
-            .text(d => "Comapnay: " + d.data.entity)
-            .attr("y", "16");
-        textblock.append("text")
-            .text(d => d.data.recordsLost)
-            .attr("y", "32");
-        textblock.append("text")
-            .text(d => d.data.year)
-            .attr("y", "48");
-    };
+    //     textblock.append("text")
+    //         .text(d => "Comapnay: " + d.data.entity)
+    //         .attr("y", "16");
+    //     textblock.append("text")
+    //         .text(d => d.data.recordsLost)
+    //         .attr("y", "32");
+    //     textblock.append("text")
+    //         .text(d => d.data.year)
+    //         .attr("y", "48");
+    // };
 
-    circle.on("click", selectOccupation)
+    // circle.on("click", selectOccupation)
 
-
-};
+// };
     
     
     
